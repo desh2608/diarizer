2fc5028119105e85402c760d359860773ffcb405
Desh Raj <r.desh26@gmail.com>
2022-01-31 08:44:47 -0500

    refactor libricss scripts

---
+++ scripts/libricss/040_overlap.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+stage=0
+
+# Overlap detector Hyperparameters (tuned on session0)
+onset=0.3
+offset=0.7
+min_duration_on=0.4
+min_duration_off=0.5
+
+. ./path.sh
+. ./utils/parse_options.sh
+
+CORPUS_DIR=/export/c01/corpora6/LibriCSS
+DATA_DIR=data/libricss
+EXP_DIR=exp/libricss
+
+mkdir -p exp
+
+if [ $stage -le 4 ]; then
+  for part in dev test; do
+    echo "Running pyannote Overlap Detection on ${part}"
+    (
+    for audio in $(ls $DATA_DIR/${part}/audios/*.wav | xargs -n 1 basename)
+    do
+      filename=$(echo "${audio}" | cut -f 1 -d '.')
+      echo ${filename} > exp/list_${filename}.txt
+      
+      utils/queue.pl -l "hostname=c*" --mem 2G \
+        $EXP_DIR/${part}/log/ovl/ovl_${filename}.log \
+        python diarizer/overlap/pyannote_overlap.py \
+          --in-dir $DATA_DIR/${part}/audios \
+          --file-list exp/list_${filename}.txt \
+          --out-dir $EXP_DIR/${part}/ovl \
+          --onset ${onset} --offset ${offset} \
+          --min-duration-on ${min_duration_on} \
+          --min-duration-off ${min_duration_off} & 
+    done
+    wait
+    )
+    rm -rf exp/list_*
+  done
+fi
+
+if [ $stage -le 5 ]; then
+  for part in dev test; do
+    echo "Evaluating ${part} overlap detector output"
+    cat $DATA_DIR/${part}/rttm/* | local/get_overlap_segments.py | grep overlap > exp/ref.rttm
+    cat $EXP_DIR/${part}/ovl/*.rttm > exp/hyp.rttm
+    ./md-eval.pl -r exp/ref.rttm -s exp/hyp.rttm |\
+      awk 'or(/MISSED SPEAKER TIME/,/FALARM SPEAKER TIME/)'
+  done
+fi
+
+exit 0
---
+++ scripts/libricss/053_spectral_ovl.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+stage=0
+
+. ./path.sh
+. ./utils/parse_options.sh
+
+CORPUS_DIR=/export/c01/corpora6/LibriCSS
+DATA_DIR=data/libricss
+EXP_DIR=exp/libricss
+
+mkdir -p exp
+
+if [ $stage -le 0 ]; then
+  for part in dev test; do
+    echo "Running spectral clustering on ${part}..."
+    (
+    for audio in $(ls $DATA_DIR/${part}/audios/*.wav | xargs -n 1 basename)
+    do
+      filename=$(echo "${audio}" | cut -f 1 -d '.')
+
+      utils/queue.pl --mem 2G $EXP_DIR/log/spectral_ovl/spectral_${filename}.log \
+        python diarizer/spectral/sclust.py \
+            --out-rttm-dir $EXP_DIR/${part}/spectral_ovl \
+            --xvec-ark-file $EXP_DIR/${part}/xvec/${filename}.ark \
+            --segments-file $EXP_DIR/${part}/xvec/${filename}.seg \
+            --overlap-rttm $EXP_DIR/${part}/ovl/${filename}.rttm \
+            --xvec-transform diarizer/models/ResNet101_16kHz/transform.h5 &
+    done
+    wait
+    )
+  done
+fi
+
+if [ $stage -le 1 ]; then
+  # Combine all RTTM files and score
+  for part in dev test; do
+    cat $DATA_DIR/${part}/rttm/*.rttm > $EXP_DIR/ref.rttm
+    cat $EXP_DIR/${part}/spectral_ovl/*.rttm > $EXP_DIR/hyp.rttm
+    LC_ALL= spyder --per-file $EXP_DIR/ref.rttm $EXP_DIR/hyp.rttm
+  done
+fi
+
+exit 0
