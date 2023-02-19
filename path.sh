808ebe7109546b80004d6ac95b71fb9340d3fd65
DTDwind <chengsam1009@gmail.com>
2023-02-19 11:32:13 +0800

    export utils in PATH

--- path.sh
+++ path.sh
@@ -4,4 +4,5 @@ export PYTHONUNBUFFERED=1
 # Activate environment
 . /home/draj/anaconda3/etc/profile.d/conda.sh && conda deactivate && conda activate vbx
 
+export PATH=${PATH}:`pwd`/utils
 export LC_ALL=C
