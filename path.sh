df99e3d4c13eb297613150dcf86643f9b3f6d77f
Desh Raj <r.desh26@gmail.com>
2024-01-12 13:42:04 -0500

    add hf token for pyannote

--- path.sh
+++ path.sh
@@ -1,6 +1,8 @@
 # Print immediately
 export PYTHONUNBUFFERED=1
 
+export PATH=${PATH}:`pwd`/utils
+
 # Activate environment
 . /home/draj/anaconda3/etc/profile.d/conda.sh && conda deactivate && conda activate vbx
 
